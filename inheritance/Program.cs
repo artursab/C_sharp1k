e660e177d995d69f6b8434a9c25692e32a59f2f2
artursab <arturs.belavskis@gmail.com>
2023-10-11 00:18:59 +0200

    inher

---
+++ inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace inheritance
+{
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"My Name is: {FirstName}");
+        }
+
+    }
+
+
+    class Student: Person
+    {
+        public void Learn()
+        {
+            Console.WriteLine("I'm learning!");
+        }
+    
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Key",
+                LastName = "Value"
+            };
+            person.PrintName();
+            Student student = new Student();
+            student.Learn();
+        }
+    }
+
+
+}
