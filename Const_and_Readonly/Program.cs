56fcb5d13ba9f8edd52bf1d7953b2c2ed9bc6796
artursab <arturs.belavskis@gmail.com>
2023-10-10 23:24:59 +0200

    const

--- Const_and_Readonly/Program.cs
+++ Const_and_Readonly/Program.cs
@@ -9,10 +9,33 @@ namespace Const_and_Readonly
     class MyClass
     {
 
+        public const double PI = 3.14;
+
+        public const string MY_ERROR = "some error";
+
+        public readonly int _a;
+
+        public static readonly int _b;
+
+        public MyClass(int a)
+        {
+            _a = a;
+        }
+
+        public void Foo()
+        {
+            Console.WriteLine(MY_ERROR);
+            Console.WriteLine(_a);
+            Console.WriteLine(_b);
+        }
+    }
+
+    class Program { 
 
         static void Main(string[] args)
         {
-
+             MyClass myClass = new MyClass(4);
+             myClass.Foo();
         }
     }
 }
---
+++ Example_const_readonly/Program.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Example
+{
+
+    class MyLogger
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}\t|INFO|\t{message}");
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}\t|ERROR|\t{message}");
+        }
+
+        public void warn(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}\t|WARN|\t{message}");
+        }
+
+    }
+
+
+
+    class Car
+    {
+        private const string ENGINE_IS_STARTED_MESSAGE = "ENGINE IS STARTED!";
+        private const string ATTEMPT_TO_START_MESSAGE = "ENGINE ATTEMPT TO START!";
+        private const string ENGINE_HAS_ALREADY_STARTED_MESSAGE = "ENGINE HAS ALREADY STARTED!";
+        private const string ATTEMPT_TO_DRIVE_MESSAGE = "ATTEMPT TO DRIVE!";
+        private const string DRIVE_MESSAGE = "MEN'S DRIVE!";
+        private const string DRIVE_ERROR_MESSAGE = "BRO, START ENGINE!";
+
+        private readonly MyLogger _myLogger;
+
+        private bool isEngineStarted;
+
+        public Car(MyLogger myLogger)
+        {
+            _myLogger = myLogger;
+        }
+
+
+
+        public void StartEngine()
+        {
+            _myLogger.Info(ATTEMPT_TO_START_MESSAGE);
+                if (isEngineStarted)
+                {
+                _myLogger.warn(ENGINE_HAS_ALREADY_STARTED_MESSAGE);
+                }
+            else
+            {
+                isEngineStarted = true;
+                _myLogger.Info(ENGINE_IS_STARTED_MESSAGE);
+            }
+        }
+
+        public void Drive()
+        {
+            _myLogger.Info(ATTEMPT_TO_DRIVE_MESSAGE);
+            if (isEngineStarted)
+            {
+                _myLogger.Info(DRIVE_MESSAGE);
+            }
+            else 
+                _myLogger.Error(DRIVE_ERROR_MESSAGE);
+        }
+
+    }
+
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car car = new Car(new MyLogger());
+            car.Drive();
+            car.StartEngine();
+            car.StartEngine();
+            car.Drive();
+        }
+    }
+}
