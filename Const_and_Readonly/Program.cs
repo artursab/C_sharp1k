a3be43940478b812f98d5d7ec1c1ccddeee3c525
artursab <arturs.belavskis@gmail.com>
2023-10-10 22:42:28 +0200

    Sharp

---
+++ Const_and_Readonly/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+namespace Const_and_Readonly
+{
+
+
+
+    class MyClass
+    {
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ PartialClass/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//partial classes
+
+namespace Part
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hab", "Mag");
+            person.PrintFullName();
+        }
+    }
+
+}
