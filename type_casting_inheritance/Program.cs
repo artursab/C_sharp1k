346d610620988c0933e26d6c1c6e84583d7b87ab
artursab <arturs.belavskis@gmail.com>
2023-10-11 21:02:50 +0200

    type

---
+++ type_casting_inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Xml.Serialization;
+
+namespace type_casting_inheritance
+{
+
+    class A
+    {
+        public int publicFiled;
+        private int privateFiled;
+        protected int protectedFiled;
+
+
+        protected int MyProperty { get; set; }
+
+        public A() 
+        {
+            Console.WriteLine(privateFiled);
+            Console.WriteLine(protectedFiled);
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine(publicFiled);
+            //Console.WriteLine(privateFiled);
+            Console.WriteLine(protectedFiled);
+        }
+    }
+
+
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                A a = new A();
+
+            Console.WriteLine(a.publicFiled);
+            //Console.WriteLine(a.privateFiled);
+            //Console.WriteLine(a.protectedFiled);
+            }
+        }
+    
+}
+
