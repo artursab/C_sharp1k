90b4bcdd3ae81a09dc81c62207a648a33369200d
artursab <arturs.belavskis@gmail.com>
2023-10-11 20:18:50 +0200

    inher

---
+++ Inheritance_example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Principal;
+
+namespace Inheritance_example
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher { FirstName = "Martin", LastName = "Dagda"};
+            Student student = new Student { FirstName = "Marta", LastName = "Adga"};
+            Person[] people = {teacher, student};
+
+            PrintPersons(people);
+        }
+
+        static void PrintPersons(Person[] people)
+        {
+            foreach (var person in people)
+            {
+                person.PrintFullName();
+            }
+        }
+    }
+}
---
+++ Inheritance_example/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_example
+{
+     class Student : Person
+    {
+        public void Learn()
+        {
+            Console.WriteLine("I'm leraning!");
+        }
+    }
+}
