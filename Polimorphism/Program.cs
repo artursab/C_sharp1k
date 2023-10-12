a11e73cd5f134cccc6c2c40ac307835f53525588
artursab <arturs.belavskis@gmail.com>
2023-10-12 11:52:14 +0200

    polimorph

---
+++ Polimorphism/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+namespace Polimorphism
+{
+
+    class Car
+    {
+
+        private void StartEngine()
+        {
+            Console.WriteLine("Engine started!");
+        }
+        public virtual void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("I'm car!");
+        }
+    }
+
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+
+    class SportCar:Car
+    {
+        public override void Drive()
+        {
+            base.Drive();
+        }
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Drive(new SportCar());
+        }
+    }
+}
