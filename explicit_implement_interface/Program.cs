5e1e2ad560ebb971d82a470775d696b7d916b9d6
artursab <arturs.belavskis@gmail.com>
2023-10-12 15:14:27 +0200

    explicit

---
+++ explicit_implement_interface/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace explicit_implement_interface
+{
+
+    interface IFirstInterface
+    {
+        void Action();
+    }
+
+    interface ISecondInterface
+    {
+        void Action();
+    }
+
+    class MyClass : IFirstInterface, ISecondInterface
+    {
+        public void Action()
+        {
+            Console.WriteLine("MyClass action...");
+        }
+    }
+
+    class MyOtherClass : IFirstInterface, ISecondInterface
+    {
+        void IFirstInterface.Action()
+        {
+            Console.WriteLine("MyOtherClass IFristInterface.Action");
+        }
+
+        void ISecondInterface.Action()
+        {
+            Console.WriteLine("MyOtherClass ISecondInterface.Action");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Foo(myClass);
+            Bar(myClass);
+
+            MyOtherClass myOtherClass = new MyOtherClass();
+            Foo(myOtherClass); 
+            Bar(myOtherClass);
+
+        }
+
+        static void Foo(IFirstInterface firstInterface)
+        {
+            firstInterface.Action();
+        }
+
+        static void Bar(ISecondInterface secondInterface)
+        {
+            secondInterface.Action();
+        }
+    }
+}
