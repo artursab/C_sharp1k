802099f33ea4b5b69ef75e7e84b3f11474d91d3a
artursab <arturs.belavskis@gmail.com>
2023-10-15 22:36:26 +0200

    box_unbox

---
+++ Boxing_unboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Boxing
+{
+
+
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+        int a = 1;
+        object b = a;
+        int c = (int)b;
+
+          //  decimal d = (decimal)b; //invalid exception
+        
+        }
+
+    }
+}
