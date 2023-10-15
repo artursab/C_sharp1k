e324c2efeaf2a9c5866fe7656599967e44ec9139
artursab <arturs.belavskis@gmail.com>
2023-10-15 23:01:36 +0200

    generics

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Generics
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            
+            Console.WriteLine($"a = {a}\t b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a}\t b = {b}");
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b; 
+            b = temp;
+        }
+    }
+}
